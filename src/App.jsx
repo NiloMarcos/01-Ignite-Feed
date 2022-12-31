c4e3dd8d986b32d58ba728c186cddc0633412a0e
Nilo Freitas <nilo.freitas@agenciam3.com>
2022-12-31 18:42:37 -0300

    finalizando a criacao da sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-import './global.css';
-
 import { Header } from './Components/Header';
 
 import { Post } from './Components/Posts'
+
+import styles from './App.module.css';
+
+import { SidebarComponent } from './Components/SidebarComponent';
 
 export function App() {
 
@@ -12,16 +14,21 @@ export function App() {
     <>
       <Header />
 
-      <Post 
-        author="Nilo Marcos" 
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, iure sint! Doloribus ad magnam cupiditate id"
-      />
-
-      <Post 
-        author="Ivan Guimarães" 
-        content="Novo post legal"
-      />
+      <div className={styles.wrapper}>
+        <SidebarComponent />
       
+        <main>
+          <Post 
+            author="Nilo Marcos" 
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, iure sint! Doloribus ad magnam cupiditate id"
+          />
+
+          <Post 
+            author="Ivan Guimarães" 
+            content="Novo post legal"
+          />
+        </main>
+      </div>
     </>
   )
 }
